b5ac30a8044772ef81ca9d1cb0257afb9fb16c47
Shashwat Rai <shashwatrai575@gmail.com>
2023-11-22 17:29:42 +0530

    shapes added

--- lib/widgets/image_picker.dart
+++ lib/widgets/image_picker.dart
@@ -15,11 +15,11 @@ class ImageInput extends StatefulWidget {
 
 class _ImageInputState extends State<ImageInput> {
   File? _storedImage;
+  String _selectedShape = 'Rectangle'; // Default shape
 
   Future<void> _takePicture() async {
     final picker = ImagePicker();
-    final imageFile =
-        await picker.pickImage(source: ImageSource.gallery, maxWidth: 600);
+    final imageFile = await picker.pickImage(source: ImageSource.gallery, maxWidth: 600);
 
     if (imageFile == null) {
       return;
@@ -31,101 +31,187 @@ class _ImageInputState extends State<ImageInput> {
 
     final appDir = await syspaths.getApplicationDocumentsDirectory();
     final fileName = path.basename(imageFile.path);
-    final savedImage =
-        await File(imageFile.path).copy('${appDir.path}/$fileName');
+    final savedImage = await File(imageFile.path).copy('${appDir.path}/$fileName');
     widget.onSelectImage(savedImage);
   }
 
   @override
   Widget build(BuildContext context) {
     return Column(
-     children: <Widget>[
-      Container(
-        width: MediaQuery.of(context).size.width*0.9,
-        height: MediaQuery.of(context).size.height*0.2,
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(10),
-          border: Border.all()
-        ),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: <Widget>[
-          Text('Upload Image',
-          style: TextStyle(
-            fontSize: 20,
-            fontStyle: FontStyle.italic
-          ),
-          ),
-          ElevatedButton(
-            onPressed: _takePicture,
-            child: Text('Choose from Device',
-            
-            style: TextStyle(
-            fontSize: 18,
-            fontStyle: FontStyle.italic
-          ),),
-          ),
-        ]),
-        alignment: Alignment.center,
-      ),
-      SizedBox(height: 25,),
-
-Container(
-          width: MediaQuery.of(context).size.width*0.9,
-          height: MediaQuery.of(context).size.height*0.5,
-          decoration: BoxDecoration(
-            border: Border.all(width: 1, color: Colors.grey),
-          ),
-          child: _storedImage != null
-              ? Image.file(
-                  _storedImage!,
-                  fit: BoxFit.cover,
-                  width: double.infinity,
-                )
-              : const Text(
-                  'No Image Taken',
-                  textAlign: TextAlign.center,
-                ),
-          alignment: Alignment.center,
-        ),
-     ],
-    );
-  }
-}  
-
-
-
-/*
-Row(
       children: <Widget>[
+        SizedBox(height: 25),
+
+      // ElevatedButton to trigger image selection
+      ElevatedButton(
+        onPressed: _takePicture,
+        child: Text(
+          'Choose from Device',
+          style: TextStyle(fontSize: 18, fontStyle: FontStyle.italic),
+        ),
+      ),
+
+      SizedBox(height: 25),
+
+        // Shape selection dropdown
+        DropdownButton<String>(
+          value: _selectedShape,
+          items: [
+            DropdownMenuItem<String>(
+              child: Text('Rectangle'),
+              value: 'Rectangle',
+            ),
+            DropdownMenuItem<String>(
+              child: Text('Square'),
+              value: 'Square',
+            ),
+            DropdownMenuItem<String>(
+              child: Text('Circle'),
+              value: 'Circle',
+            ),
+            DropdownMenuItem<String>(
+              child: Text('Heart'),
+              value: 'Heart',
+            ),
+            DropdownMenuItem<String>(
+              child: Text('Original'),
+              value: 'Original',
+            ),
+          ],
+          onChanged: (newShape) {
+            setState(() {
+              _selectedShape = newShape!;
+            });
+          },
+        ),
+
+        SizedBox(height: 25),
+
+        // Image container with shape applied
         Container(
-          width: 100,
-          height: 100,
+          width: MediaQuery.of(context).size.width * 0.9,
+          height: MediaQuery.of(context).size.height * 0.5,
           decoration: BoxDecoration(
             border: Border.all(width: 1, color: Colors.grey),
           ),
           child: _storedImage != null
-              ? Image.file(
-                  _storedImage!,
-                  fit: BoxFit.cover,
-                  width: double.infinity,
-                )
+              ? _renderImageWithSelectedShape() // Apply shape-specific mask or clip path
               : const Text(
                   'No Image Taken',
                   textAlign: TextAlign.center,
                 ),
           alignment: Alignment.center,
         ),
-        const SizedBox(
-          width: 10,
-        ),
-        Expanded(
-          child: TextButton.icon(
-            onPressed: _takePicture,
-            icon: const Icon(Icons.camera),
-            label: const Text('Take Picture'),
-          ),
-        )
       ],
     );
-    */
+  }
+
+  Widget _renderImageWithSelectedShape() {
+    switch (_selectedShape) {
+      case 'Rectangle':
+        return Image.file(
+          _storedImage!,
+          fit: BoxFit.cover,
+          width: double.infinity,
+        );
+
+      case 'Square':
+        return ClipRect(
+          child: Image.file(
+            _storedImage!,
+            fit: BoxFit.cover,
+            width: double.infinity,
+          ),
+        );
+
+      case 'Circle':
+        return ClipOval(
+          child: Image.file(
+            _storedImage!,
+            fit: BoxFit.cover,
+            width: double.infinity,
+          ),
+        );
+
+      case 'Heart':
+ case 'Heart':
+  return ClipPath(
+    clipper: HeartClipper(),
+    child: Image.file(
+      _storedImage!,
+      fit: BoxFit.cover,
+      width: double.infinity,
+    ),
+  );
+
+
+      default:
+        return Image.file(
+          _storedImage!,
+          fit: BoxFit.cover,
+          width: double.infinity,
+        );
+    }
+  }
+}
+
+
+class HeartClipper extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    final Path path = Path();
+    path.moveTo(size.width / 2, size.height / 5);
+
+    path.cubicTo(
+      size.width / 2,
+      size.height / 5 - 30,
+      size.width / 2 - 40,
+      size.height / 5 - 80,
+      size.width / 2 - 80,
+      size.height / 5 - 80,
+    );
+
+    path.cubicTo(
+      size.width / 2 - 120,
+      size.height / 5 - 80,
+      size.width / 2 - 120,
+      size.height / 5 - 40,
+      size.width / 2 - 120,
+      size.height / 5,
+    );
+
+    path.cubicTo(
+      size.width / 2 - 120,
+      size.height / 5 + 40,
+      size.width / 2 - 80,
+      size.height / 5 + 80,
+      size.width / 2,
+      size.height / 5 + 120,
+    );
+
+    path.cubicTo(
+      size.width / 2 + 80,
+      size.height / 5 + 80,
+      size.width / 2 + 120,
+      size.height / 5 + 40,
+      size.width / 2 + 120,
+      size.height / 5,
+    );
+
+    path.cubicTo(
+      size.width / 2 + 120,
+      size.height / 5 - 40,
+      size.width / 2 + 80,
+      size.height / 5 - 80,
+      size.width / 2,
+      size.height / 5 - 120,
+    );
+
+    path.close();
+    return path;
+  }
+
+  @override
+  bool shouldReclip(CustomClipper<Path> oldClipper) {
+    return false;
+  }
+}
